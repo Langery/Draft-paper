4cd0ecb12432215712a51bc28dcbe0cd8ab5995e
Langery <1245066197@qq.com>
2019-10-18 17:51:22 +0800

    change some code

--- gulp-text/README.md
+++ gulp-text/README.md
@@ -2,3 +2,14 @@
 
 > Link：[Gulp 中文地址](https://www.gulpjs.com.cn/)
 
+> unfinish
+
+- [ ]  待续...
+
+## Note
+
+1. 四个默认API
+- gulp.task()
+- gulp.src()
+- gulp.dest()
+- gulp.watch()
--- gulp-text/gulpfile.js
+++ gulp-text/gulpfile.js
@@ -1,4 +1,10 @@
-function defaultTask (cb) {
-	cb()
-}
-exports.default = defaultTask
+// function defaultTask (cb) {
+// 	cb()
+// }
+// exports.default = defaultTask
+
+var gulp = require('gulp')
+
+gulp.task('default', () => {
+	console.log('Hello World!')
+})
